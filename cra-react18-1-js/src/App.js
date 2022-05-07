d9a5a86bf037f3f16912cdc30a7e684eef5267b0
Momtchil Momtchev <momtchil@momtchev.com>
2022-05-07 12:13:33 +0200

    use a more thorough test

--- cra-react18-1-js/src/App.js
+++ cra-react18-1-js/src/App.js
@@ -1,17 +1,79 @@
+import React from 'react';
 import './App.css';
-import 'ol/ol.css';
+
 import { fromLonLat } from 'ol/proj';
-import * as rl from 'rlayers';
+import { Polygon, Point } from 'ol/geom';
+
+import { RMap, ROSM, RLayerVector, RFeature, RPopup } from 'rlayers';
+import { RStyle, RFill, RStroke } from 'rlayers/style';
+
+import 'ol/ol.css';
+
+const center = fromLonLat([2.364, 48.82]);
+
+const coords = {
+  origin: [2.364, 48.82],
+  ArcDeTriomphe: [2.295, 48.8737],
+  PlaceDItalie: [2.355, 48.831],
+  Bastille: [2.369, 48.853],
+  TourEiffel: [2.294, 48.858],
+  Montmartre: [2.342, 48.887]
+};
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <rl.RMap width={'100%'} height={'60vh'} initial={{center: fromLonLat([2.364, 48.82]), zoom: 11}}>
-          <rl.ROSM />
-        </rl.RMap>
-      </header>
-    </div>
+    <RMap className='example-map' initial={{ center, zoom: 11 }}>
+      <ROSM />
+      <RLayerVector zIndex={10}>
+        <RFeature geometry={new Point(fromLonLat(coords.ArcDeTriomphe))}>
+          <RStyle>
+            <RStroke color='yellow' width={4} />
+            <RFill color='transparent' />
+          </RStyle>
+          <RPopup trigger={'click'} className='example-overlay'>
+            <div className='card'>
+              <p className='card-header'>
+                <strong>Arc de Triomphe</strong>
+              </p>
+              <p className='card-body text-center'>Popup on click</p>
+            </div>
+          </RPopup>
+        </RFeature>
+        <RFeature
+          geometry={
+            new Polygon([
+              [
+                fromLonLat(coords.PlaceDItalie),
+                fromLonLat(coords.Bastille),
+                fromLonLat(coords.TourEiffel),
+                fromLonLat(coords.PlaceDItalie)
+              ]
+            ])
+          }
+          onClick={React.useCallback(
+            (e) =>
+              e.map.getView().fit(e.target.getGeometry().getExtent(), {
+                duration: 250
+              }),
+            []
+          )}
+        >
+          <RStyle>
+            <RStroke color='yellow' width={4} />
+            <RFill color='transparent' />
+          </RStyle>
+          <RPopup trigger={'hover'} className='example-overlay'>
+            <p>
+              <strong>Les catacombes</strong>
+            </p>
+            <p>
+              <em>Popup on hover, pan on click</em>
+            </p>
+          </RPopup>
+        </RFeature>
+      </RLayerVector>
+    </RMap>
   );
 }
 
