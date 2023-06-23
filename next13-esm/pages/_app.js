e256af67241d7142a83260b8d473eb7b1e9a53ff
Momtchil Momtchev <momtchil@momtchev.com>
2023-06-23 19:42:28 +0200

    add next13-esm test

---
+++ next13-esm/pages/_app.js
@@ -0,0 +1,12 @@
+import 'ol/ol.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+MyApp.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  return { ...appProps }
+};
+
+export default MyApp;
